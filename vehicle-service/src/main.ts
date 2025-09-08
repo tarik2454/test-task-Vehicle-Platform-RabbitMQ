aa4f0415548a74a81b2136ba98be40bda5d6e962
Taras Kovalyk <tarik2454@gmail.com>
2025-09-08 20:25:46 +0300

    refactor: update CORS configuration in user and vehicle services to allow all origins for improved flexibility

--- vehicle-service/src/main.ts
+++ vehicle-service/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
